ef4b883650c268f67b196acae55e8c219bbdccba
Feriolet <veincent.yap@gmail.com>
2024-03-23 14:01:02 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,34 +1,16 @@
+
 from setuptools import find_packages, setup
-from os import path
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-setuptools.setup(
+setup(
     name="dimorphite_dl",
-    version=dimorphite_dl.__version__,
-    author="Patrick J. Ropp, Jesse C. Kaminsky, Sara Yablonski & Jacob D. Durrant",
-    author_email="durrantj@pitt.edu",
-    description="Dimorphite-DL: an open-source program for enumerating the ionization states of drug-like small molecules",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Feriolet/dimorphite_dl",
+    version="1.2.4",
+    description="Dimorphite-DL adds hydrogen atoms to molecular representations, as appropriate for a user-specified pH range. It is a fast, accurate, accessible, and modular open-source program for enumerating small-molecule ionization states.",
+    author="Durrant Lab",
+    url="https://github.com/durrantlab/dimorphite_dl/",
+    install_requires=["rdkit", "numpy", "scipy", "mpi4py", "setuptools"],
     packages=find_packages(),
     package_data={
         "": ["*.smi", "*.smarts", "training_data/*.json"]
     },
     include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "Topic :: Scientific/Engineering :: Chemistry"
-    ],
-    python_requires='>=3.6',
-    extras_require={
-        'rdkit': ['rdkit>=2017.09'],
-    },
-    entry_points={'console_scripts':
-                      ['dimorphite_dl = dimorphite_dl.run_dock:main']}
 )
